358e037c818c11db72f96773fa60a12e41b598a6
KKazuki <kazuki.k.bl8@gmail.com>
2019-07-01 15:48:40 +0900

    nginx + uWSGI + Flask + line-bot-sdk リプライ送信まで完了。bs4イインストール後、使用クラス作成まで

---
+++ app/src/run.py
@@ -0,0 +1,59 @@
+from flask import Flask, request, abort
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
+
+import logging
+
+logging.basicConfig(filename='/var/logs/python.log', level=logging.DEBUG)
+
+app = Flask(__name__)
+
+line_bot_api = LineBotApi('bB/emTb7qCaeqWMUZB8QFNaDKfjX/WaqGXzhefUbR/ekqj31/yRneAY/HJn06iBiOR1mqlhuda9A1343d4ZBDGjuv6QsLPczGtMV5iSzFE249IEWX2q280JPZd1pxNkfuNqdl6tHZzp35QuX6ailHgdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('323db4ca5a11c147b072e2b4faa3d3d5')
+
+@app.route("/")
+def index():
+    app.logger.info('call index')
+    return '<h1>index</h1>'
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    app.logger.debug('Debug call /call back')
+    
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        print("Invalid signature. Please check your channel access token/channel secret.")
+        abort(400)
+
+    return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+
+    msg = 'れちゃん「{}」'.format(event.message.text)
+    
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=msg))
+
+
+if __name__ == "__main__":
+    app.run()
