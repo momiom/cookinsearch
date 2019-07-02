ec98644c2d4944d812653320d619f2e4fd4432c2
Kazuki Kawasaki <kazuki.k.bl8@gmail.com>
2019-07-03 08:32:49 +0900

    dom解析テスト用スクリプト作成。flex_messageのサンプルjson作成

--- app/src/bs4scrach.py
+++ app/src/bs4scrach.py
@@ -1,1 +1,23 @@
 from bs4 import  BeautifulSoup
+import requests
+
+def request(input_word):
+        # 検索用url作成
+        base_url = 'https://cookien.com/?s={}'
+        request_url = base_url.format(input_word)
+        print(request_url)
+        
+        # リクエストして成功したらbs4に渡す
+        response = requests.get(request_url)
+        print(response.status_code)
+        body = response.text
+        if response.status_code == requests.codes.ok:
+            soup = BeautifulSoup(body, 'html.parser')
+            return soup
+        else:
+            response.raise_for_status()
+            
+
+if __name__ == "__main__":
+    soup = request('鶏肉')
+    print(soup)
