1bb1f2c3ab8d765942e2d31e5445f52d1d0cb491
Kazuki Kawasaki <kazuki.k.bl8@gmail.com>
2019-07-03 00:22:30 +0900

    add itms class.

---
+++ app/src/items.py
@@ -0,0 +1,9 @@
+from abc import ABCMeta, abstractmethod
+
+class Items(metaclass = ABCMeta):
+    def __init__():
+        pass
+    
+class CookinItem(Items):
+    def __init__():
+        super()
--- app/src/scrapers.py
+++ app/src/scrapers.py
@@ -1,26 +1,40 @@
+from abc import ABCMeta, abstractmethod
 from bs4 import BeautifulSoup
 import requests
 import logging
 
 
-class Scraper:
+class Scraper(metaclass=ABCMeta):
     def __init__(self, input_word):
         if __name__ != "__main__":
             # Flaskのロガーを取得
             self.logger = logging.getLogger('flask.app')
         # 検索ワードを設定
         self.input_word = input_word
+        self.base_url = ''
+
+    @abstractmethod
+    def get_items():
+        pass
+
+    @abstractmethod
+    def request(self):
+        pass
 
 
 class CookinScraper(Scraper):
+    def get_items():
+        
+        return cokkin_items
+    
     def request(self):
         # 検索用url作成
         base_url = 'https://cookien.com/?s={}'
-        request_url = base_url.format(self.input_word)
-        self.logger.info('Request URL: {}'.format(request_url))
+        self.request_url = base_url.format(self.input_word)
+        self.logger.info('Request URL: {}'.format(self.request_url))
 
         # リクエストして成功したらbs4に渡す
-        response = requests.get(request_url)
+        response = requests.get(selfrequest_url)
         body = response.text
         if response.status_code == requests.codes.ok:
             self.soup = BeautifulSoup(body, 'lxml')
@@ -31,8 +45,10 @@ class CookinScraper(Scraper):
 
 class ShirogohanScraper(Scraper):
     def request(self):
+        pass
 
 
 if __name__ == "__main__":
     s = CookinScraper('鶏肉')
     s.request_cokkin()
+
