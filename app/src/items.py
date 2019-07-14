a358f3bde88afaf4b517633ff8d23024a0463bc0
K-Kazuki <kazuki.k.bl8@gmail.com>
2019-07-14 14:45:33 +0900

    bs4scratchをscraperに実装 itemsを実装

--- app/src/items.py
+++ app/src/items.py
@@ -1,11 +1,15 @@
 from abc import ABCMeta, abstractmethod
 
 
-class Items(metaclass=ABCMeta):
-    def __init__(self):
-        pass
+class Item(metaclass=ABCMeta):
+    def __init__(self, values):
+        self.url = values['url']
+        self.title = values['title']
+        self.img = values['img']
+        self.tags = values['tags']
 
 
-class CookinItem(Items):
-    def __init__(self):
-        super().__init__()
+class CookinItem(Item):
+    def __init__(self, values):
+        super().__init__(values)
+        self.cooking_method = values['cooking_method']
--- app/src/scrapers.py
+++ app/src/scrapers.py
@@ -1,7 +1,9 @@
+import re
 from abc import ABCMeta, abstractmethod, ABC
 from bs4 import BeautifulSoup
 import requests
 import logging
+from . import items
 
 
 class Scraper(metaclass=ABCMeta):
@@ -25,9 +27,6 @@ class Scraper(metaclass=ABCMeta):
 
 
 class CookinScraper(Scraper):
-    def get_items(self):
-        return cokkin_items
-
     def request(self):
         # 検索用url作成
         base_url = 'https://cookien.com/?s={}'
@@ -38,10 +37,36 @@ class CookinScraper(Scraper):
         response = requests.get(self.request_url)
         body = response.text
         if response.status_code == requests.codes.ok:
-            soup = BeautifulSoup(body, 'lxml')
+            self._soup = BeautifulSoup(body, 'lxml')
         else:
             self.logger.warning('Failed to request.')
             response.raise_for_status()
+
+    def get_items(self):
+        post_items = []
+
+        post_articles = self._soup.find_all('article', id=re.compile(r'post-[0-9]{1,5}'))
+        for post in post_articles:
+            url = post.find('a').get('href')
+            title = post.find('h2', class_='entry-title-2').string
+            img = post.find('img').get('src')
+            tags = post.find('div', class_='content_tag').find_all('span', recursive=False)
+            tags = [tag.get_text().strip() for tag in tags]
+            # 1つだけの場合「すぐめし」アイコンを探す
+            if len(tags) == 1:
+                has_sugumeshi = len(post.find_all('div', class_='cat_now')) > 0
+                if has_sugumeshi:
+                    tags.append('すぐめし')
+            cooking_methods = post.find('div', class_='cooking_method').find_all('span', recursive=False)
+            cooking_methods = [method.get_text().strip() for method in cooking_methods]
+            post_items.append(items.CookinItem({
+                'url': url.strip(),
+                'title': title.strip(),
+                'img': img.strip(),
+                'tags': tags,
+                'cooking_methods': cooking_methods
+            }))
+        return post_items
 
 
 class ShirogohanScraper(Scraper):
